fd68d64b0d0b8a986b00ed3c14f3b2bf8a30670e
shouston3 <shouston3@me.com>
2017-07-22 19:16:03 +0100

    Main is a counter which increments by 2 for each click

--- Main.elm
+++ Main.elm
@@ -6,40 +6,33 @@ import Html.Events exposing (onClick)
 -- MODEL
 
 type alias Model =
-  Bool
+  Int
 
 init : ( Model, Cmd Msg )
 init =
-  ( False, Cmd.none )
+  ( 0, Cmd.none )
 
 -- MESSAGES
 
 type Msg
-  = Expand
-  | Collapse
+  = Increment Int
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  if model then
-    div []
-        [ button [ onClick Collapse ] [ text "Collapse" ]
-        , text "Widget"
-        ]
-  else
-    div []
-      [ button [ onClick Expand ] [ text "Expand" ] ]
+  div []
+      [ button [ onClick (Increment 2) ] [ text "+" ]
+      , text (toString model)
+      ]
 
 -- UPDATE
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Expand ->
-      ( True, Cmd.none )
-    Collapse ->
-      ( False, Cmd.none )
+    Increment howMuch ->
+      ( model + howMuch, Cmd.none )
 
 -- SUBSCRIPTIONS
 
